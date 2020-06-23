7d7476d823817f4e00fc55e22ec891a358f790b3
EC2 Default User <ec2-user@ip-172-31-36-149.ap-northeast-1.compute.internal>
2020-06-23 13:49:47 +0000

    20200623 add_and_change_columns_to_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :department, length: { in: 2..30 }, allow_blank: true
+  validates :affiliation, length: { in: 2..30 }, allow_blank: true
   validates :basic_time, presence: true
   validates :work_time, presence: true
   has_secure_password
@@ -59,7 +59,7 @@ class User < ApplicationRecord
   # 更新を許可するカラムを定義
   def self.updatable_attributes
     
-    ["name", "email","department", "password"]
+    ["name", "email","affiliation", "password"]
   end
   
 end
