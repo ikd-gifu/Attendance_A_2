e6dd1be4a41686004ad7791f10f7c0db034f5fc1
EC2 Default User <ec2-user@ip-172-31-36-149.ap-northeast-1.compute.internal>
2020-08-10 14:34:32 +0000

    20200810 overtime_designated_work_end_time

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -8,15 +8,19 @@ module AttendancesHelper
     return false
   end
 
-  def working_times(start, finish)
-    format("%.2f", (((finish - start) / 60) / 60.0))
-  end
-  
-  def overtime_next_day(start, finish, next_day, scheduled_end_time)
-    if next_day == "true"
+  def working_times(start, finish, next_day)
+    if next_day == true
       format("%.2f", (((finish - start) / 60) / 60.0) + 24)
     else
-      format("%.2f", (((finish - start) / 60) / 60.0))
+     format("%.2f", (((finish - start) / 60) / 60.0))
+    end
+  end
+  
+  def overtime(designated_work_end_time, next_day, scheduled_end_time, worked_on)
+    if next_day == true
+      format("%.2f", (((scheduled_end_time - worked_on.to_datetime.in_time_zone.change(hour: 17, min: 30, sec: 0)) / 60) / 60.0) + 24)
+    else
+      format("%.2f", (((scheduled_end_time - worked_on.to_datetime.in_time_zone.change(hour: 17, min: 30, sec: 0)) / 60) / 60.0))
     end
   end
   
