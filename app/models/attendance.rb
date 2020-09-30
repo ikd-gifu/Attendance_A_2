b67964482631fe45ebc4f53239ab378daabf2908
EC2 Default User <ec2-user@ip-172-31-36-149.ap-northeast-1.compute.internal>
2020-09-30 13:47:15 +0000

    20200930 attendance validation

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -18,6 +18,9 @@ class Attendance < ApplicationRecord
 
   validate :finished_at_is_invalid_without_a_started_at
   validate :started_at_than_finished_at_fast_if_invalid
+  
+  # 退勤時間が存在しない場合、出勤時間は無効
+  validate :started_at_is_invalid_without_a_finished_at
 
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -26,6 +29,12 @@ class Attendance < ApplicationRecord
   def started_at_than_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
       errors.add(:started_at, "より早い退勤時間は無効です") if started_at > finished_at
+    end
+  end
+  
+  def started_at_is_invalid_without_a_finished_at
+    unless Date.current == worked_on #今日の日付と取得された日付が一致するか
+    errors.add(:finished_at, "が必要です") if finished_at.blank? && started_at.present? #今日の日付と同じ日付でない場合エラー出力
     end
   end
 end
