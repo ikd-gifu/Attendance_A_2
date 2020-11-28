1c3f008f6eea744d7bfb314ac2683cb3ae427015
EC2 Default User <ec2-user@ip-172-31-36-149.ap-northeast-1.compute.internal>
2020-11-28 13:51:43 +0000

    20200028 attendance change log branch

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -268,10 +268,32 @@ class AttendancesController < ApplicationController
   
   def attendance_modifying_log
     # debugger
-    @first_day = params[:date].to_date.beginning_of_month
-    @last_day = @first_day.end_of_month
-    @attendance = @user.attendances.where(worked_on: @first_day..@last_day)
-    @attendance_logs = @attendance.where(user_id: @user.id, attendance_change_application_status: "承認")
+    if params[:date].present?
+      # unless params[:date][:year].present?
+        @first_day = params[:date].to_date.beginning_of_month
+        @last_day = @first_day.end_of_month
+        @attendance = @user.attendances.where(worked_on: @first_day..@last_day)
+        @attendance_logs = @attendance.where(user_id: @user.id, attendance_change_application_status: "承認")
+      # end
+    elsif params[:date][:year].present? && params[:date][:month].present?
+      params[:date] = params[:date][:year] + "-" + params[:date][:month] + "-" + "01"
+      @first_day = params[:date].to_date.beginning_of_month
+      @last_day = @first_day.end_of_month
+      @attendance = @user.attendances.where(worked_on: @first_day..@last_day)
+      @attendance_logs = @attendance.where(user_id: @user.id, attendance_change_application_status: "承認")
+    end
+    # if params[:date][:year].present? && params[:date][:month].present?
+    #   params[:date] = params[:date][:year] + "-" + params[:date][:month] + "-" + "01"
+    #   @first_day = params[:date].to_date.beginning_of_month
+    #   @last_day = @first_day.end_of_month
+    #   @attendance = @user.attendances.where(worked_on: @first_day..@last_day)
+    #   @attendance_logs = @attendance.where(user_id: @user.id, attendance_change_application_status: "承認")
+    # else
+    #   @first_day = params[:date].to_date.beginning_of_month
+    #   @last_day = @first_day.end_of_month
+    #   @attendance = @user.attendances.where(worked_on: @first_day..@last_day)
+    #   @attendance_logs = @attendance.where(user_id: @user.id, attendance_change_application_status: "承認")
+    # end
   end
 
   private
