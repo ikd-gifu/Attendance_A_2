55ccc0e44e9a1193a75a7d38d54fb7f25dc159de
ikd-gifu <oki19ki@gmail.com>
2021-01-12 14:45:03 +0000

    20210112 Attendance_A correct_9th

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -298,7 +298,7 @@ class AttendancesController < ApplicationController
 
   def attendance_change_application_confirmation_show #勤怠変更申請の確認リンク
     @user = User.find(params[:id])
-    @attendance = Attendance.find(params[:id])
+    # @attendance = Attendance.find(params[:id])
     @first_day = params[:date].to_date.beginning_of_month
     @last_day = @first_day.end_of_month
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
